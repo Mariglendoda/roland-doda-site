daf4bc531951a0bc62ab65ab5c2c005255d5f33f
Mariglen Doda <mariglendoda1999@gmail.com>
2022-09-28 14:34:02 +0200

    adding icons on top form section

--- src/components/contact-section/Contact.vue
+++ src/components/contact-section/Contact.vue
@@ -1,33 +1,52 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div id="contact" class="grid content-center items-center gap-[20px]">
-    <form class="grid justify-end gap-[24px]">
-      <div class="flex gap-[14px]">
-        <input
-          class="rgba w-[350px] h-[60px] outline-none accent-[rgba(255, 255, 255, 0.1)] p-3 rounded-10 bg-[rgba(255, 255, 255, 0.1)]"
-          type="text"
-          placeholder="Your Name"
-        />
-        <input
-          class="rgba w-[350px] h-[60px] outline-none accent-[rgba(255, 255, 255, 0.1)] p-3 rounded-10"
-          type="text"
-          placeholder="Your Email Address"
-        />
+  <div id="contact" class="grid content-center">
+    <div class="grid gap-[20px]">
+      <div class="grid grid-cols-[auto,auto,auto] justify-center gap-[120px]">
+        <div class="grid justify-items-center gap-[10px]">
+          <img src="../../../public/assets/images/phone.png" alt="" />
+          <div>+355676457173</div>
+        </div>
+
+        <div class="grid justify-items-center gap-[10px]">
+          <img src="../../../public/assets/images/gmail.png" alt="" />
+          <div>rolanddoda2014@gmail.com</div>
+        </div>
+
+        <div class="grid justify-items-center gap-[10px]">
+          <img src="../../../public/assets/images/location.png" alt="" />
+          <div>Tirana, Albania</div>
+        </div>
       </div>
 
-      <input
-        class="rgba w-[714px] h-[60px] outline-none accent-[rgba(255, 255, 255, 0.1)] p-3 rounded-10"
-        type="text"
-        placeholder="Your Subject"
-      />
-      <input
-        class="rgba w-[714px] pb-[173px] grid content-start outline-none accent-[rgba(255, 255, 255, 0.1)] p-3 rounded-10"
-        type="text"
-        placeholder="Your Message..."
-      />
+      <form class="grid content-center justify-center gap-[24px]">
+        <div class="flex gap-[14px]">
+          <input
+            class="rgba w-[350px] h-[60px] outline-none accent-[rgba(255, 255, 255, 0.1)] p-3 rounded-10 bg-[rgba(255, 255, 255, 0.1)]"
+            type="text"
+            placeholder="Your Name"
+          />
+          <input
+            class="rgba w-[350px] h-[60px] outline-none accent-[rgba(255, 255, 255, 0.1)] p-3 rounded-10"
+            type="text"
+            placeholder="Your Email Address"
+          />
+        </div>
 
-      <button class="bg-glassmorphism rounded-10 w-[159px] h-[60px]">Send Message</button>
-    </form>
+        <input
+          class="rgba w-[714px] h-[60px] outline-none accent-[rgba(255, 255, 255, 0.1)] p-3 rounded-10"
+          type="text"
+          placeholder="Your Subject"
+        />
+        <input
+          class="rgba w-[714px] pb-[173px] grid content-start outline-none accent-[rgba(255, 255, 255, 0.1)] p-3 rounded-10"
+          type="text"
+          placeholder="Your Message..."
+        />
+
+        <button class="bg-glassmorphism rounded-10 w-[159px] h-[60px]">Send Message</button>
+      </form>
+    </div>
   </div>
 </template>
