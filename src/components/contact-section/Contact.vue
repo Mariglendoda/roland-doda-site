17b9bf7ee6bc6b2a1748a8e70fda430f25df13f3
Mariglen Doda <mariglendoda1999@gmail.com>
2022-09-28 10:58:26 +0200

    implementing form contact

--- src/components/contact-section/Contact.vue
+++ src/components/contact-section/Contact.vue
@@ -1,37 +1,33 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div id="contact" class="grid content-start items-center gap-[20px]">
-    <div class="grid content-center w-[400px]">
-      <div class="relative grid content-center items-center w-[100px]">
-        <div class="absolute right-[0] box w-[63px] h-[42px] bg-graystarry -z-10 skew-x-[30deg]"></div>
-        <div class="subtitle text-orange">Contact Info</div>
+  <div id="contact" class="grid content-center items-center gap-[20px]">
+    <form class="grid justify-end gap-[24px]">
+      <div class="flex gap-[14px]">
+        <input
+          class="rgba w-[350px] h-[60px] outline-none accent-[rgba(255, 255, 255, 0.1)] p-3 rounded-10 bg-[rgba(255, 255, 255, 0.1)]"
+          type="text"
+          placeholder="Your Name"
+        />
+        <input
+          class="rgba w-[350px] h-[60px] outline-none accent-[rgba(255, 255, 255, 0.1)] p-3 rounded-10"
+          type="text"
+          placeholder="Your Email Address"
+        />
       </div>
 
-      <div class="h1">Contact Me</div>
+      <input
+        class="rgba w-[714px] h-[60px] outline-none accent-[rgba(255, 255, 255, 0.1)] p-3 rounded-10"
+        type="text"
+        placeholder="Your Subject"
+      />
+      <input
+        class="rgba w-[714px] pb-[173px] grid content-start outline-none accent-[rgba(255, 255, 255, 0.1)] p-3 rounded-10"
+        type="text"
+        placeholder="Your Message..."
+      />
 
-      <div class="description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu donec dui massa scelerisque felis consectetur.
-        Duis vel urna ut ullamcorper velit non non. A purus faucibus velit ac natoque.
-      </div>
-    </div>
-
-    <div class="grid gap-[10px]">
-      <img src="../../../public/assets/images/notifications.png" alt="" />
-
-      <div class="grid grid-flow-col">
-        <div class="grid align-center justify-start align-center relative">
-          <div class="circle w-[30px] h-[30px] bg-[#D36A6B] blur-[20px] rounded-full"></div>
-          <img class="absolute" src="../../../public/assets/images/notification.png" alt="" />
-        </div>
-
-        <div class="grid align-center justify-start align-center relative">
-          <div class="circle w-[30px] h-[30px] bg-[#F7FBFF] blur-[20px] rounded-full"></div>
-          <img class="absolute" src="../../../public/assets/images/comments.png" alt="" />
-        </div>
-      </div>
-
-      <img class="absolute" src="../../../public/assets/images/ilustration.png" alt="" />
-    </div>
+      <button class="bg-glassmorphism rounded-10 w-[159px] h-[60px]">Send Message</button>
+    </form>
   </div>
 </template>
