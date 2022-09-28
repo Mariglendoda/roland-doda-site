dcff196d661250a8adf399578bcd8d5367d2d3c4
Mariglen Doda <mariglendoda1999@gmail.com>
2022-09-28 16:04:23 +0200

    finish contact page

--- src/components/contact-section/Contact.vue
+++ src/components/contact-section/Contact.vue
@@ -1,8 +1,42 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div id="contact" class="grid content-center">
-    <div class="grid gap-[20px]">
+  <div id="contact" class="grid grid-cols-[auto_1fr] items-start content-center">
+    <div class="grid content-center gap-[20px]">
+      <div>
+        <div class="relative grid content-center items-center w-[100px]">
+          <div class="absolute right-0 w-[63px] h-[42px] bg-graystarry -z-10 skew-x-[30deg]"></div>
+          <div class="subtitle text-orange">Contact Info</div>
+        </div>
+
+        <div class="h1">Contact Me</div>
+
+        <div class="description w-[400px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu donec dui massa scelerisque felis consectetur.
+          Duis vel urna ut ullamcorper velit non non. A purus faucibus velit ac natoque.
+        </div>
+      </div>
+
+      <div class="grid justify-start gap-[10px]">
+        <img src="../../../public/assets/images/notifications.png" alt="" />
+
+        <div class="grid">
+          <div class="grid align-center justify-start align-center relative left-12 top-10">
+            <div class="circle w-[30px] h-[30px] bg-[#D36A6B] blur-[20px] rounded-full"></div>
+            <img class="absolute" src="../../../public/assets/images/notification.png" alt="" />
+          </div>
+
+          <div class="grid align-center justify-start align-center relative -top-10 left-[370px]">
+            <div class="circle w-[30px] h-[30px] bg-[#F7FBFF] blur-[20px] rounded-full"></div>
+            <img class="absolute" src="../../../public/assets/images/comments.png" alt="" />
+          </div>
+        </div>
+
+        <img class="absolute" src="../../../public/assets/images/ilustration.png" alt="" />
+      </div>
+    </div>
+
+    <div class="grid justify-end gap-[20px]">
       <div class="grid grid-cols-[auto,auto,auto] justify-center gap-[120px]">
         <div class="grid justify-items-center gap-[10px]">
           <img src="../../../public/assets/images/phone.png" alt="" />
