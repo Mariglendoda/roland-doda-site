bf75800d5f418948ae5690382bed96deb620bacd
Mariglen Doda <mariglendoda1999@gmail.com>
2022-09-30 13:23:23 +0200

    creating refactoring code

--- src/components/testimonials-section/Testimonials.vue
+++ src/components/testimonials-section/Testimonials.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue"
+import Recommendation from "./Recommendation.vue"
 
 const recommendations = ref([
   {
@@ -162,92 +163,11 @@ const recommendations = ref([
       </div>
 
       <div class="grid grid-cols-3 gap-[20px]">
-        <div class="grid gap-[20px] content-start max-w-[400px]">
-          <div
-            class="grid gap-[10px] bg-glassmorphism p-4 rounded-[20px]"
-            v-for="recommendation of recommendations.slice(0, 4)"
-          >
-            <div class="flex items-center gap-[15px]">
-              <div
-                class="img w-[50px] h-[50px] rounded-full bg-cover"
-                :style="{ backgroundImage: ` url(${recommendation.image})` }"
-              ></div>
+        <Recommendation :data="recommendations.slice(0, 4)" />
 
-              <div>
-                <div class="description">{{ recommendation.fullName }}</div>
-                <div class="subtitle">{{ recommendation.job }}</div>
-              </div>
-            </div>
+        <Recommendation :data="recommendations.slice(4, 7)" />
 
-            <div class="subtitle">
-              {{ recommendation.description }}
-            </div>
-
-            <div class="subtitle">
-              <div class="date">
-                {{ recommendation.date.day }} {{ recommendation.date.month }} {{ recommendation.date.year }}
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="grid gap-[20px] content-start max-w-[400px]">
-          <div
-            class="grid gap-[10px] bg-glassmorphism p-4 rounded-[20px]"
-            v-for="recommendation of recommendations.slice(4, 7)"
-          >
-            <div class="flex items-center gap-[15px]">
-              <div
-                class="img w-[50px] h-[50px] rounded-full bg-cover"
-                :style="{ backgroundImage: ` url(${recommendation.image})` }"
-              ></div>
-
-              <div>
-                <div class="description">{{ recommendation.fullName }}</div>
-                <div class="subtitle">{{ recommendation.job }}</div>
-              </div>
-            </div>
-
-            <div class="subtitle">
-              {{ recommendation.description }}
-            </div>
-
-            <div class="subtitle">
-              <div class="date">
-                {{ recommendation.date.day }} {{ recommendation.date.month }} {{ recommendation.date.year }}
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="grid gap-[20px] content-start max-w-[400px]">
-          <div
-            class="grid gap-[10px] bg-glassmorphism p-4 rounded-[20px]"
-            v-for="recommendation of recommendations.slice(6)"
-          >
-            <div class="flex items-center gap-[15px]">
-              <div
-                class="img w-[50px] h-[50px] rounded-full bg-cover"
-                :style="{ backgroundImage: ` url(${recommendation.image})` }"
-              ></div>
-
-              <div>
-                <div class="description">{{ recommendation.fullName }}</div>
-                <div class="subtitle">{{ recommendation.job }}</div>
-              </div>
-            </div>
-
-            <div class="subtitle">
-              {{ recommendation.description }}
-            </div>
-
-            <div class="subtitle">
-              <div class="date">
-                {{ recommendation.date.day }} {{ recommendation.date.month }} {{ recommendation.date.year }}
-              </div>
-            </div>
-          </div>
-        </div>
+        <Recommendation :data="recommendations.slice(7)" />
       </div>
 
       <div class="absolute bottom-0 w-full grid justify-center content-center">
