b073b6e6a15877c21cc499402d626fe00b67361c
Roland Doda <rolanddoda2014@gmail.com>
2022-10-08 12:39:17 +0200

    Finish left section

--- src/components/home-section/Left.vue
+++ src/components/home-section/Left.vue
@@ -1,45 +1,48 @@
 <script setup lang="ts">
 import LineIcon from "./svg/Line.vue"
-import StackOverflowIcon from "./svg/StackOverflow.vue"
 import GithubIcon from "./svg/Github.vue"
-import DevtoIcon from "./svg/Devto.vue"
-import Github2Icon from "./svg/Github2.vue"
+import DevelopingTheFutureIcon from "./svg/DevelopingTheFuture.vue"
 
 const stats = [
-  { iconComponent: StackOverflowIcon, text: "~21,000 reputation" },
-  { iconComponent: GithubIcon, text: "47 followers" },
-  { iconComponent: DevtoIcon, text: "50,000+ total post views" },
+  { text: "💻 Experienced Javascript Developer." },
+  { text: "💘 VueJs & SvelteKit Lover." },
+  { text: "👨‍ Front-end enthusiast" },
+  { text: "‍💼 Vue Consultant." },
+  { text: "🗃️ Modular and scalable Frontend architectures" },
 ]
 </script>
 
 <template>
-  <div class="grid content-center">
-    <div class="h2 text-style w-[380px]">Hi 👋 my name is Roland Doda.</div>
+  <div class="flex gap-4">
+    <DevelopingTheFutureIcon />
 
-    <div class="relative">
-      <div class="h1 text-style w-[510px]">A Professional Frontend Developer</div>
-      <LineIcon class="absolute top-[62px] left-[100px]" />
-    </div>
+    <div class="grid grid-rows-[auto,1fr,1fr,auto] gap-3">
+      <div class="h2 text-style w-[380px]">Hi 👋 my name is Roland Doda.</div>
 
-    <div class="grid gap-1">
-      <div v-for="stat of stats" :key="stat.text" class="flex items-center gap-1">
-        <component :is="stat.iconComponent" />
-        <div>{{ stat.text }}</div>
-      </div>
-    </div>
-
-    <div class="flex gap-4 mt-3">
-      <div
-        class="flex flex-row justify-center justify-items-center items-center w-[308px] bg-glassmorphism rounded-[10px]"
-      >
-        <Github2Icon />
-
-        <button class="p bg-gradient w-[235px] h-[60px] rounded-[10px] text-primary capitalize">
-          check out my gitHub
-        </button>
+      <div class="relative">
+        <div class="h1 text-style w-[510px]">I'm a Professional Frontend Developer</div>
+        <LineIcon class="absolute top-[62px] left-[140px]" />
       </div>
 
-      <button class="p w-[161px] h-[63px] bg-glassmorphism rounded-[10px] capitalize">download CV</button>
+      <div class="grid gap-1">
+        <div v-for="stat of stats" :key="stat.text" class="flex items-center gap-1">
+          <div class="description">{{ stat.text }}</div>
+        </div>
+      </div>
+
+      <div class="flex gap-4">
+        <div
+          class="flex flex-row justify-center justify-items-center items-center w-[308px] bg-glassmorphism rounded-[10px]"
+        >
+          <GithubIcon />
+
+          <button class="p bg-gradient w-[235px] h-[60px] rounded-[10px] text-primary capitalize">
+            check out my gitHub
+          </button>
+        </div>
+
+        <button class="p w-[161px] h-[63px] bg-glassmorphism rounded-[10px] capitalize">download CV</button>
+      </div>
     </div>
   </div>
 </template>
