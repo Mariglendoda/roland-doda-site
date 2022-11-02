f0e7bf92a7a32ab4347c1d927e5aabeda239d2e7
Mariglen Doda <mariglendoda1999@gmail.com>
2022-11-02 18:20:07 +0100

    styling article cards & changing text

--- src/components/articles-section/Articles.vue
+++ src/components/articles-section/Articles.vue
@@ -4,31 +4,27 @@ import { ref } from "vue"
 const articles = ref([
   {
     title: "🔙 🤔 I remember a few years...",
-    description:
-      "Despite all these opinions and my colleagues trying to push me into Angular and React, I bet 100% on Vue.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et faucibus viverraasdasasd...",
     href: "",
-    img: "assets/images/vite.png",
+    img: "assets/images/img1.png",
   },
   {
     title: "🍍 Pinia! The recommended...",
-    description:
-      "In this article, we are going to learn how to reset stores that use function/setup syntax by creating a Pinia plugin!.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et faucibus viverraasdasasd...",
     href: "",
-    img: "assets/images/pinia.png",
+    img: "assets/images/img2.png",
   },
   {
     title: "🥳 Let's build an interactive...",
-    description:
-      "For the best experience reading and following the article, each heading has a branch on Github and can be seen online on Stackblitz. 💯",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et faucibus viverraasdasasd...",
     href: "",
-    img: "assets/images/svg.png",
+    img: "assets/images/img3.png",
   },
   {
     title: "🚀 Deploy to Github Pages...",
-    description:
-      "Despite all these opinions and my colleagues trying to push me into Angular and React, I bet 100% on Vue.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et faucibus viverraasdasasd...",
     href: "",
-    img: "assets/images/githubpage.png",
+    img: "assets/images/img4.png",
   },
 ])
 </script>
@@ -57,14 +53,62 @@ const articles = ref([
       </div>
     </div>
 
-    <div class="overflow-x-auto hide-scrollbar grid grid-flow-col justify-start gap-6 items-center">
-      <div class="grid gap-[10px] w-[350px]" v-for="article of articles">
-        <img :src="article.img" alt="" />
+    <div class="relative overflow-x-auto hide-scrollbar grid grid-flow-col justify-start gap-6 items-center">
+      <div class="grid grid-flow-col justify-start gap-6 items-center">
+        <div v-for="article of articles">
+          <div class="w-[380px] h-[283px] rounded-t-[12px]">
+            <img class="rounded-t-[12px]" :src="article.img" :alt="article.title" />
+          </div>
 
-        <div class="h3">{{ article.title }}</div>
+          <div class="card">
+            <div class="h3">{{ article.title }}</div>
 
-        <div class="ellipsis-3">{{ article.description }}</div>
+            <div class="w-[332px] h-[48px] ellipsis-3 subtitle text-[#80A4C2]">
+              {{ article.description }}
+            </div>
+
+            <div class="footer-work__card grid grid-cols-[auto,1fr] items-center w-[100%] gap-1">
+              <button class="btn">
+                <p>Read Article</p>
+
+                <svg width="24" height="24" viewBox="0 0 24 24">
+                  <path fill="currentColor" d="M8.59 16.59L13.17 12L8.59 7.41L10 6l6 6l-6 6l-1.41-1.41z" />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <div class="grid absolute right-0 w-[100px] h-[100%] bg-linear-card"></div>
     </div>
   </section>
 </template>
+
+<style>
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 20px 24px;
+  gap: 10px;
+  width: 380px;
+  background: linear-gradient(180.67deg, rgba(255, 255, 255, 0.4) -136.79%, rgba(255, 255, 255, 0.1) 49.83%);
+  backdrop-filter: blur(45px);
+  border-radius: 0px 0px 12px 12px;
+}
+
+.btn {
+  display: grid;
+  grid-auto-flow: column;
+  align-items: center;
+  align-content: center;
+  padding: 12px;
+  width: 100%;
+  height: 41px;
+  background: linear-gradient(168deg, rgba(255, 255, 255, 0.4) 7.84%, rgba(255, 255, 255, 0.1) 92.16%);
+  border-radius: 6px;
+  font-size: 14px;
+}
+</style>
