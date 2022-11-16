29e6b0d524a59134eee95910af46c8d6ccc1d504
Mariglen Doda <mariglendoda1999@gmail.com>
2022-11-16 13:51:51 +0100

    Icons convert in single component svg

---
+++ public/assets/svg/CursorSvg.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M5.43359 5.33337L14.8603 28L18.2069 18.1467L28.1003 14.76L5.43359 5.33337Z"
+        stroke="white"
+        stroke-width="2.66667"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </div>
+</template>
