22e4810531bf7ac9fc5f3a83f1095c3fa6ff938c
Mariglen Doda <mariglendoda1999@gmail.com>
2022-11-16 19:19:59 +0100

    implementing single component for social-media

--- src/components/home-section/Home.vue
+++ src/components/home-section/Home.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
 import { hrefs } from "../../constants"
 // Components
+import Header from "@components/Header.vue"
 import LeftSection from "./Left.vue"
 import RightSection from "./Right.vue"
-// import RightSection from "./Right.vue"
-import Header from "@components/Header.vue"
+import SocialMedia from "./SocialMedia.vue"
 
 // social media links
-import CardGithub from "./svg/CardGithub.vue"
-import CardTwitter from "./svg/CardTwitter.vue"
-import CardLinkedin from "./svg/CardLinkedin.vue"
-import CardStackoverflow from "./svg/CardStackoverflow.vue"
-import CardDevTo from "./svg/CardDevTo.vue"
 </script>
 
 <template>
@@ -24,25 +19,6 @@ import CardDevTo from "./svg/CardDevTo.vue"
       <RightSection />
     </div>
 
-    <div class="mb-[30px] socials flex gap-6 items-center">
-      <a href="https://github.com/Rolanddoda">
-        <CardGithub />
-      </a>
-
-      <a href="https://twitter.com/dev_rolandi">
-        <CardTwitter />
-      </a>
-
-      <a href="https://www.linkedin.com/in/roland-doda/">
-        <CardLinkedin />
-      </a>
-      <a href="https://stackoverflow.com/users/6385184/roland">
-        <CardStackoverflow />
-      </a>
-
-      <a href="https://dev.to/the_one">
-        <CardDevTo />
-      </a>
-    </div>
+    <SocialMedia />
   </section>
 </template>
---
+++ src/components/home-section/SocialMedia.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { hrefs } from "../../constants"
+// Components
+
+// social media links
+import CardGithub from "./svg/CardGithub.vue"
+import CardTwitter from "./svg/CardTwitter.vue"
+import CardLinkedin from "./svg/CardLinkedin.vue"
+import CardStackoverflow from "./svg/CardStackoverflow.vue"
+import CardDevTo from "./svg/CardDevTo.vue"
+</script>
+
+<template>
+  <div class="mb-[30px] socials flex gap-6 items-center">
+    <a href="https://github.com/Rolanddoda">
+      <CardGithub />
+    </a>
+
+    <a href="https://twitter.com/dev_rolandi">
+      <CardTwitter />
+    </a>
+
+    <a href="https://www.linkedin.com/in/roland-doda/">
+      <CardLinkedin />
+    </a>
+    <a href="https://stackoverflow.com/users/6385184/roland">
+      <CardStackoverflow />
+    </a>
+
+    <a href="https://dev.to/the_one">
+      <CardDevTo />
+    </a>
+  </div>
+</template>
