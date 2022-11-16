f9bf6c181b92957406bd006d0dc834701287854e
Mariglen Doda <mariglendoda1999@gmail.com>
2022-11-16 10:29:04 +0100

    adding figma icons

--- src/components/home-section/Right.vue
+++ src/components/home-section/Right.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import VueIcon from "./svg/VueIcon.vue"
+import TypescriptIcon from "./svg/TypescriptIcon.vue"
+import FigmaIcon from "./svg/FigmaIcon.vue"
 </script>
 
 <template>
   <div class="right-section grid justify-center items-center relative">
     <div class="w-[406px] h-[406px] border border-white rounded-full absolute left-0 right-0 m-auto">
       <VueIcon />
+
+      <FigmaIcon />
     </div>
-    <div class="w-[314px] h-[314px] border border-white rounded-full absolute left-0 right-0 m-auto"></div>
+
+    <div class="w-[314px] h-[314px] border border-white rounded-full absolute left-0 right-0 m-auto">
+      <TypescriptIcon />
+    </div>
   </div>
 </template>
