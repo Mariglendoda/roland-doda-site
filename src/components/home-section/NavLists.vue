6bb3970bd35ba966e995943ca0afc4d5c3ce955c
Mariglen Doda <mariglendoda1999@gmail.com>
2022-12-23 22:00:40 +0100

    implementing socials with url-links

--- src/components/home-section/NavLists.vue
+++ src/components/home-section/NavLists.vue
@@ -1,25 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SocialsMedia from "./SocialsMedia.vue"
+</script>
 
 <template>
-  <div class="navs grid grid-flow-col justify-end items-center bg-secondary rounded-[30px] w-full p-2">
-    <div class="nav-item bg-white px-[39px] py-[13px] rounded-[30px] text-primary">
-      <a href="#">Home</a>
+  <div class="grid grid-flow-col items-center gap-6">
+    <div class="navigation-list grid grid-flow-col justify-end items-center bg-secondary rounded-[30px] w-full p-2">
+      <div class="nav-item bg-white px-[39px] py-[13px] rounded-[30px] text-primary">
+        <a href="#">Home</a>
+      </div>
+
+      <div class="nav-item px-[39px] py-[13px]">
+        <a href="#">About</a>
+      </div>
+
+      <div class="nav-item px-[39px] py-[13px]">
+        <a href="#">Works</a>
+      </div>
+
+      <div class="nav-item px-[39px] py-[13px]">
+        <a href="#">Blog</a>
+      </div>
+
+      <div class="nav-item px-[39px] py-[13px]">
+        <a href="#">Contact</a>
+      </div>
     </div>
 
-    <div class="nav-item px-[39px] py-[13px]">
-      <a href="#">About</a>
-    </div>
-
-    <div class="nav-item px-[39px] py-[13px]">
-      <a href="#">Works</a>
-    </div>
-
-    <div class="nav-item px-[39px] py-[13px]">
-      <a href="#">Blog</a>
-    </div>
-
-    <div class="nav-item px-[39px] py-[13px]">
-      <a href="#">Contact</a>
-    </div>
+    <SocialsMedia />
   </div>
 </template>
---
+++ src/components/home-section/SocialsMedia.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import LinkedinSvg from "./svg/LinkedinSvg.vue"
+import TwitterSvg from "./svg/TwitterSvg.vue"
+import GithubSvg from "./svg/GithubSvg.vue"
+</script>
+
+<template>
+  <div class="grid grid-flow-col gap-5 items-center">
+    <a href="https://github.com/Rolanddoda">
+      <GithubSvg />
+    </a>
+
+    <a href="https://twitter.com/DodaMariglen">
+      <TwitterSvg />
+    </a>
+
+    <a href="https://www.linkedin.com/in/roland-doda/">
+      <LinkedinSvg />
+    </a>
+  </div>
+</template>
